65089282ce2cf4966df48ddad15e1dd5830ceac3
SAMUEL NDEMAKI <T37391232@kra.gov>
2023-03-22 09:36:33 +0300

    additional changes on all 3 files

--- script.js
+++ script.js
@@ -8,9 +8,25 @@ const calculatePowerBtn = document.getElementById('calculate_wealth');
 
 let data = [];
 
+getRandomUser();
+getRandomUser();
+getRandomUser();
+getRandomUser();
+
 //fetch random users and add money
 
-function getRandomUser() {
-    fetch('https://randomuser.me/api');
+async function getRandomUser() {
+    const res = await fetch('https://randomuser.me/api');
+    const data = await res.json();
 
-}
+    const user = data.results[0];
+
+    const newUser = {
+        name: `${user.name.first} ${user.name.last}`,
+        money: Math.floor(Math.random() * 1000000)
+    };
+
+    console.log(newUser);
+    }
+
+
