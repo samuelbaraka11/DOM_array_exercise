cd93adeb86cbc3150f6ba88d6e76f34d7a73516e
SAMUEL NDEMAKI <T37391232@kra.gov>
2023-03-22 08:17:40 +0300

    additional js changes

--- script.js
+++ script.js
@@ -13,3 +13,16 @@ function getRandomModel() {
     fetch('https://api-ninjas.com/api/cars');
 
 }
+// var model = 'volkswagen'                                          
+// $.ajax({
+//     method: 'GET',
+//     url: 'https://api.api-ninjas.com/v1/cars?model=' + model,
+//     headers: { 'X-Api-Key': 'YOUR_API_KEY'},
+//     contentType: 'application/json',
+//     success: function(result) {
+//         console.log(result);
+//     },
+//     error: function ajaxError(jqXHR) {
+//         console.error('Error: ', jqXHR.responseText);
+//     }
+// });
